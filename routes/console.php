f9eb7deebd2d8494acd181a70fabeeeba5b1d35e
Nozo <Nozo00qq@gmail.com>
2024-08-15 01:55:46 +0300

    fixing bugs

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+
+// this command to make a user as admin
+Artisan::command('make-admin {userId}', function ($userId) {
+    $user = User::find($userId);
+    if($user) {
+        $user->is_admin = 1;
+        $user->save();
+    } else {
+        abort(404);
+    }
+
+    $this->info("User {$userId} has been made an admin.");
+})->describe('Make a user an admin');
